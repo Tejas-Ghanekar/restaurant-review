397cf3801636b72827103d4bcd47a9a061d8c19f
Tejas.Ghanekar <tejas.ghanekar@slalom.com>
2019-12-20 13:45:34 -0500

    added try block

--- lambda/getRestaurants.py
+++ lambda/getRestaurants.py
@@ -15,12 +15,14 @@ password = os.environ['PASSWORD']
 db_name = os.environ['DB_NAME']
 
 def get_restaurants(event,context):
-  result=[]
-  conn = pymysql.connect(host,user=user_name,passwd=password,db=db_name,connect_timeout=5)
-  with conn.cursor() as cur:
-    cur.execute("""select * from restaurant""")
-    rows=cur.fetchall()
+  try:
+    result=[]
+    conn = pymysql.connect(host,user=user_name,passwd=password,db=db_name,connect_timeout=5)
+    with conn.cursor() as cur:
+      cur.execute("""select * from restaurant""")
+      rows=cur.fetchall()
+      for row in rows:
+        result.append({'id':row[0], 'name':row[1], 'address':row[2], 'city':row[3], 'state':row[4], 'area':row[5], 'postal_code':row[6], 'phone':row[7], 'price':row[8]})
+  finally:
     cur.close()
-    for row in rows:
-      result.append({'id':row[0], 'name':row[1], 'address':row[2], 'city':row[3], 'state':row[4], 'area':row[5], 'postal_code':row[6], 'phone':row[7], 'price':row[8]})
   return result
